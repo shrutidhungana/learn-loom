8fe2d5fefbb0007296fbc2ced45c70cb485d0f8d
shrutidhungana <shrutidhungana123@gmail.com>
2025-03-10 14:56:36 +0545

    Backend and postgres setup complete

---
+++ server/database.ts
@@ -0,0 +1,21 @@
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: Number(process.env.POSTGRES_PORT) || 5432,
+});
+
+pool
+  .connect()
+  .then(() => console.log("🚀 Connected to PostgreSQL"))
+  .catch((err) => console.error("❌ PostgreSQL Connection Error:", err));
+
+export default pool;
