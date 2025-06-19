ba1df9318e140d099d675f9f4a6d48a1ba8a4706
shrutidhungana <shrutidhungana123@gmail.com>
2025-06-19 14:25:33 +0545

    Created /usr/bin/bash4 not found page: 1)To acknowledge any page that might not be there and entered wrong route

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>LEARN LOOM</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Learning Management System" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
--- server/server.ts
+++ server/server.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ?? 5000;
 
 app.get("/", async (req, res) => {
   try {
